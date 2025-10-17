0a4d74e96fdc86edf7b94ba047576dbcf9876ddf
Gert Stalpaert <gertst@gmail.com>
2025-10-17 11:48:27 +0200

    publish pages

---
+++ docs/_nuxt/DdYCTV-1.js
@@ -0,0 +1,6 @@
+import{x as r,y as a,z as s,W as n}from"./BhWtVLlM.js";const l={class:"min-h-screen bg-cover bg-center bg-fixed relative",style:{background:`linear-gradient(
+        135deg,
+        var(--sporty-blue-600) 0%,
+        var(--sporty-blue-200) 50%,
+        var(--sporty-blue-500) 100%
+      )`}},o={class:"relative"},c={__name:"default",setup(i){return(t,e)=>(a(),r("div",l,[e[0]||(e[0]=s("div",{class:"absolute inset-0 bg-white/10"},null,-1)),s("div",o,[n(t.$slots,"default")])]))}};export{c as default};
